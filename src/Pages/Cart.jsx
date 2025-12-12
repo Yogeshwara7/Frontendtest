c7aabc3a908cc3389af5767e2fbfcdc90085ad0b
Yogeshwara B <yogeshwara567@gmail.com>
2025-12-12 23:55:49 +0530

    card style in Cart.jsx file enhanced it

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,26 +1,44 @@
 import { useContext } from "react";
 import { CartContext } from "../context/CardContext";
 
-function Cart(){
-    const{cart} = useContext(CartContext);
-    console.log("Cart items:", cart);
-    return(
-        <div style={{padding:"20px"}}>
-            <h2>Cart Items</h2>
-            {cart.length===0 ? (
-                <p>No items in the cart</p>
-            ) : (  cart.map((item, index)=>(
-                <div key={index} style={{border:"1px solid gray", margin:"10px", padding:"10px", display:"flex", gap:"20px", alignItems:"center"}}>
-                    <img src={item.image} alt={item.name} style={{width:"150px", height:"150px", objectFit:"cover", border:"1px solid gray", padding:"6px", backgroundColor:"#fff", borderRadius:"4px"}}/>
-                    <div>
-                        <h3>{item.name}</h3>
-                        <p>Price: {item.price}Rs</p>
-                    </div>
-                </div>
-            ))
-            )}
+function Cart() {
+  const { cart } = useContext(CartContext);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2 style={{ textAlign: "center" }}>Cart Items</h2>
+
+      {cart.length === 0 ? (
+        <p style={{ textAlign: "center" }}>No items in the cart</p>
+      ) : (
+        <div
+          style={{
+            display: "flex",flexWrap: "wrap",gap: "20px",justifyContent: "center",marginTop: "20px"
+          }}
+        >
+          {cart.map((item, index) => (
+            <div
+              key={index}
+              style={{
+                width: "250px",border: "1px solid gray",borderRadius: "10px",padding: "15px",backgroundColor: "#f0f8ff",display: "flex",flexDirection: "column",alignItems: "center"
+              }}
+            >
+              <img
+                src={item.image}
+                alt={item.name}
+                style={{
+                  width: "200px",height: "200px", objectFit: "cover", borderRadius: "10px",border: "1px solid gray",marginBottom: "10px"
+                }}
+              />
+
+              <h3 style={{ margin: "5px 0" }}>{item.name}</h3>
+              <p style={{ margin: 0 }}>Price: {item.price} Rs</p>
+            </div>
+          ))}
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default Cart;
