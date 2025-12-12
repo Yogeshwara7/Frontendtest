f47249daaa493a78ed34e395618e9b0c2512a51a
Yogeshwara B <yogeshwara567@gmail.com>
2025-12-12 23:47:16 +0530

    build error in app,jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './Pages/Home';
-import Cart from './Pages/cart';
+import Cart from './Pages/Cart';
 import { CartProvider } from './context/CardContext';
 
 function App() {
--- src/components/Flowercard.jsx
+++ src/components/Flowercard.jsx
@@ -17,7 +17,7 @@ function FlowerCard({flower}){
             backgroundColor:"#f9f9f9",
         }}>
         
-        <img src={flower.image} style={{width:"200px", height:"200px"}}/>
+        <img src={flower.image} alt={flower.name} style={{width:"200px", height:"200px", objectFit:"cover", border:"1px solid gray", padding:"6px", backgroundColor:"#fff", borderRadius:"4px"}}/>
         <h3>{flower.name}</h3>
         <p>Price: {flower.price}Rs</p>
        <button style={{color:"white", backgroundColor:"grey", cursor:"pointer"}} onClick={() => {
